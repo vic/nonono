2d98d2361de8520e76488701954f5e2af083048d
Victor Hugo Borja <vborja@apache.org>
2022-03-14 12:28:09 -0600

    Rename parameter names

--- nonono-lib/src-3/nonono/NoNoNo.scala
+++ nonono-lib/src-3/nonono/NoNoNo.scala
@@ -1,6 +1,6 @@
 package nonono
 
 sealed trait NoNoNo:
-  def apply[T](f: T => Any)(cause: String): Nothing = ???
+  def apply[T](prevent: T => Any)(hint: String): Nothing = ???
 
 object NoNoNo extends NoNoNo
